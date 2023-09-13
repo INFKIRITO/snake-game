9272bc9161881890a31c2ead5f1f432c7f5846f1
INFKIRITO <94058048+INFKIRITO@users.noreply.github.com>
2023-09-13 11:08:30 +0530

    initial commit

---
+++ src/com/solution/gameframe.java
@@ -0,0 +1,20 @@
+package com.solution;
+
+import javax.swing.JFrame;
+
+public class gameframe extends JFrame{
+
+
+    private static final long serialVersionUID = 1L;
+
+    gameframe() {
+        gamepanel panel = new gamepanel();
+        this.add(panel);
+        this.setTitle("snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
---
+++ src/com/solution/snakegame.java
@@ -0,0 +1,9 @@
+package com.solution;
+
+public class snakegame {
+
+    public static void main(String[] args) {
+        new gameframe();
+    }
+
+}
